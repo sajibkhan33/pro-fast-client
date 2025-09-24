3056f24bcbcd68ec3eb601c1e03dc6e853ee9deb
sajibkhan33 <nurnavi929@gmail.com>
2025-09-25 04:07:26 +0600

    creating logIn and register page , authentication done with firebase and updating nav

---
+++ src/router/router.jsx
@@ -0,0 +1,39 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/Home/Home/Home";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Authentication/Login/Login";
+import Register from "../pages/Authentication/Register/Register";
+import Coverage from "../pages/Coverage/Coverage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: 'coverage',
+        Component: Coverage,
+        loader: () => fetch('./serviceCenter.json')
+      }
+    ]
+  },
+  {
+    path: '/',
+    Component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        Component: Login
+      },
+      {
+        path: 'register',
+        Component: Register
+      }
+    ]
+  }
+]);
